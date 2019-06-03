0dd7b8972bbb3fc608db52aac0299aa99101dfad
Khang <invisibledark3@gmail.com>
2019-06-03 21:02:44 +0700

    little change app.js

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use((req,res,next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
     if (req.method === "OPTIONS")
         {
+          res.header("Access-Control-Allow-Origin", "*");
           res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
           return res.status(200).json();
         }
