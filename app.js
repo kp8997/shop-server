31d9cb2a6e087a35d9f915321e582ded610ebf3f
Khang <invisibledark3@gmail.com>
2019-06-04 23:33:56 +0700

    fix app.js

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ db.on('error', (error) => {
 
 // ----------- CORS --------------------------
 app.use((req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
   res.header('Access-Control-Allow-Credentials', true);
   // res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
