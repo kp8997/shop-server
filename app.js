0ce408b9e2e4df2875826e9277c24daf8b360b1a
Khang <invisibledark3@gmail.com>
2019-06-04 16:12:50 +0700

    fixxxxxxxxxxxx

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ db.on('error', (error) => {
 app.use((req,res,next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+  res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === "OPTIONS") {
           res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
           return res.status(200).json({});
