15d47d6ceaa35e1e608ff9651f06c64cf0451968
Khang <invisibledark3@gmail.com>
2019-06-04 15:41:48 +0700

    set header fix app.js

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ db.on('error', (error) => {
 
 // ----------- CORS --------------------------
 app.use((req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
     if (req.method === "OPTIONS") {
-          res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
+          res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
           return res.status(200).json({});
     }
   next();
