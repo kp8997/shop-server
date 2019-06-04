970e4ff61bdaa2ed477617aaaf6e4b6e9129399f
Khang <invisibledark3@gmail.com>
2019-06-04 23:24:05 +0700

    fix app.js

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ app.use((req,res,next) => {
       res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
           return res.status(200).json({});
     }
+    if (req.method === "POST") {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+      res.header('Access-Control-Allow-Credentials', true);
+    }
   next();
 });
 
