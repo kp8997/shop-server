f59273b40650f2d046a34e5803bdba7abe975871
Khang <invisibledark3@gmail.com>
2019-06-05 00:11:13 +0700

    add fix cors

--- app.js
+++ app.js
@@ -28,23 +28,23 @@ db.on('error', (error) => {
 });
 
 // ----------- CORS --------------------------
-app.use((req,res,next) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
-  res.header('Access-Control-Allow-Credentials', true);
-  // res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
-          return res.status(200).json({});
-    }
-    if (req.method === "POST") {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
-      res.header('Access-Control-Allow-Credentials', true);
-    }
-  next();
-});
+// app.use((req,res,next) => {
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+//   res.header('Access-Control-Allow-Credentials', true);
+//   // res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
+//     if (req.method === "OPTIONS") {
+//       res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
+//           return res.status(200).json({});
+//     }
+//     if (req.method === "POST") {
+//       res.header("Access-Control-Allow-Origin", "*");
+//       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+//       res.header('Access-Control-Allow-Credentials', true);
+//     }
+//   next();
+// });
 
 // const corsOption = {
 //   origin: '*',
