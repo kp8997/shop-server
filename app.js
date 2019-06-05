ecce6fee96971f3c4e0bc21fc8a4e3a62ecc80da
Khang <invisibledark3@gmail.com>
2019-06-05 23:46:28 +0700

    fixxxx

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ db.on('error', (error) => {
 // ----------- CORS --------------------------
 app.use((req,res,next) => {
   // res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
   res.header('Access-Control-Allow-Credentials', true);
   // res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
@@ -38,11 +38,11 @@ app.use((req,res,next) => {
       res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
           return res.status(200).json({});
     }
-    if (req.method === "POST") {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
-      res.header('Access-Control-Allow-Credentials', true);
-    }
+    // if (req.method === "POST") {
+    //   res.header("Access-Control-Allow-Origin", "*");
+    //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+    //   res.header('Access-Control-Allow-Credentials', true);
+    // }
   next();
 });
 
